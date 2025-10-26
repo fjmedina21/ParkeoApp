3fb139a48981b40e0b29e226c314028d4bc7c0a5
Francisco Medina <fjmedina21@outlook.com>
2025-10-26 09:14:34 -0400

    for deploy

--- ParkeoApp.Api/Program.cs
+++ ParkeoApp.Api/Program.cs
@@ -12,8 +12,6 @@ using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
-builder.WebHost.UseUrls($"http://*:{port}");
 builder.Host.UseSerilog((hostBuilderContext, loggerConfig) => loggerConfig.ReadFrom.Configuration(hostBuilderContext.Configuration));
 
 // Add services to the container.
